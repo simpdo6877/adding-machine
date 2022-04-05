298155fa47f522e4b1471569868d5eed0bf327d3
simpdo6877 <93728625+simpdo6877@users.noreply.github.com>
2022-04-05 11:53:37 -0400

    Add files via upload

--- file92.py
+++ file92.py
@@ -1,4 +1,10 @@
-first_number=int(input('what is the first number you want to add?'))
-second_number=int(input('waht is the second numberyou want to add?'))
-sum = first_number+second_number
-print('the sum of the two numbers is',sum)
+
+while True:
+    try:
+        first_number=int(input('what is the first number you want to add?'))
+        second_number=int(input('waht is the second numberyou want to add?'))
+        sum = first_number+second_number
+        print('the sum of the two numbers is',sum)
+        break
+    except:
+        print('Your silly. That is not and integer...')
