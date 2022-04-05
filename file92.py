72d601a0dcd7c22f086e580759512f57709548c5
simpdo6877 <93728625+simpdo6877@users.noreply.github.com>
2022-04-05 11:43:39 -0400

    Add files via upload

---
+++ file92.py
@@ -0,0 +1,4 @@
+first_number=int(input('what is the first number you want to add?'))
+second_number=int(input('waht is the second numberyou want to add?'))
+sum = first_number+second_number
+print('the sum of the two numbers is',sum)
